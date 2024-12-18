eb3db92164dfd86096ec7b7d5a7bf5f7fd37fad1
vinay3git <24mp2254@rit.ac.in>
2024-12-18 15:59:48 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -4,41 +4,41 @@ int n, i, j, u, v, a, b;
 int cost[10][10], visited[10]= {0}, min, mincost= 0, ne= 1;
 
 void main() {
-        printf("\nEnter the number of nodes: ");
-        scanf("%d", &n);
+	printf("\nEnter the number of nodes: ");
+	scanf("%d", &n);
 
-        printf("\nEnter the adjacency matrix:\n");
-        for (i = 1; i <= n; i++) {
-                for (j = 1; j <= n; j++) {
-                        scanf("%d", &cost[i][j]);
-                        if (cost[i][j] == 0) {
-                                cost[i][j] = 999;
-                        }
-                }
-        }
+	printf("\nEnter the adjacency matrix:\n");
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			scanf("%d", &cost[i][j]);
+			if (cost[i][j] == 0) {
+				cost[i][j] = 999;
+			}
+		}
+	}
 
-        visited[1] = 1;
-        printf("\n");
+	visited[0] = 1;
+	printf("\n");
 
-        while (ne < n) {
-                for (i = 1, min = 999; i <= n; i++) {
-                        for (j = 1; j <= n; j++) {
-                                if (cost[i][j] < min && visited[i] != 0) {
-                                        min = cost[i][j];
-                                        a = u = i;
-                                        b = v = j;
-                                }
-                        }
-                }
+	while (ne < n) {
+		for (i = 0, min = 999; i < n; i++) {
+			for (j = 0; j < n; j++) {
+				if (cost[i][j] < min && visited[i] != 0) {
+ 					min = cost[i][j];
+					a = u = i;
+					b = v = j;
+				}
+			}
+		}
 
-                if (visited[u] == 0 || visited[v] == 0) {
-                        printf("\nEdge %d: (%d %d) cost: %d", ne++, a, b, min);
-                        mincost += min;
-                        visited[b] = 1;
-                }
+		if (visited[u] == 0 || visited[v] == 0) {
+			printf("\nEdge %d: (%d %d) cost: %d", ne++, a, b, min);
+			mincost += min;
+			visited[b] = 1;
+		}
 
-                cost[a][b] = cost[b][a] = 999; // Mark the edge as used
-        }
+		cost[a][b] = cost[b][a] = 999; // Mark the edge as used
+	}
 
-        printf("\n\nMinimum cost: %d\n", mincost);
+	printf("\n\nMinimum cost: %d\n", mincost);
 }
